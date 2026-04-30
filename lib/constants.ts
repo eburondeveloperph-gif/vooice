36acb931e3f80111196b9bed86595fc1e68dc7a9
eburondeveloperph <eburondeveloperph@gmail.com>
2026-04-30 08:04:35 +0800

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -22,7 +22,7 @@
  * Default Live API model to use
  */
 export const DEFAULT_LIVE_API_MODEL =
-  'gemini-2.5-flash-native-audio-preview-09-2025';
+  'gemini-3.1-flash-live-preview';
 
 export const DEFAULT_VOICE = 'Aoede';
 
